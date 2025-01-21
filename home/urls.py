c060ba2903b6f24f3d1ba98c7a378121683277e6
Rômulo Franco Uchôa <romulofrancovi@gmail.com>
2025-01-20 22:57:30 -0300

    atualização

--- home/urls.py
+++ home/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('servico/', include("servico.urls"))
+    path('servico/', include("servico.urls")),
+    path('access/', include("access.urls")),
 ]
